c330394074633d45c944f3e6874d528a5d21fa1c
Peter <pikepa@users.noreply.github.com>
2020-12-16 09:12:46 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,26 +5,26 @@ namespace Pikepa\ContactMe\Tests;
 use Pikepa\ContactMe\ContactMeServiceProvider;
 
  class TestCase extends \Orchestra\Testbench\TestCase
-{
-    public function setUp(): void
-    {
-        parent::setUp();
-        // additional setup
-    }
+ {
+     public function setUp(): void
+     {
+         parent::setUp();
+         // additional setup
+     }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-            ContactMeServiceProvider::class,
-        ];
-    }
+     protected function getPackageProviders($app)
+     {
+         return [
+             ContactMeServiceProvider::class,
+         ];
+     }
 
-    protected function getEnvironmentSetUp($app)
-    {
-        // import the CreateMessagesTable class from the migration
-        include_once __DIR__.'/../database/migrations/create_messages_table.php.stub';
+     protected function getEnvironmentSetUp($app)
+     {
+         // import the CreateMessagesTable class from the migration
+         include_once __DIR__.'/../database/migrations/create_messages_table.php.stub';
 
-        // run the up() method of that migration class
-        (new \CreateMessagesTable)->up();
-    }
-}
+         // run the up() method of that migration class
+         (new \CreateMessagesTable)->up();
+     }
+ }
